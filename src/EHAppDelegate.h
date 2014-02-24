46c3d06b8f422ec345f824b8c650760fb81d22cc
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-02-24 21:35:18 +0100

    Adds user config and other setup stuff from iOS. Refs #2.

--- src/EHAppDelegate.h
+++ src/EHAppDelegate.h
@@ -6,5 +6,18 @@
 
 @end
 
+extern NSString *decimal_separator_changed;
+extern NSString *did_accept_file;
+extern NSString *did_accept_file_path;
+extern NSString *did_add_row;
+extern NSString *did_add_row_pos;
+extern NSString *did_change_changelog_version;
+extern NSString *did_import_csv;
+extern NSString *did_remove_row;
+extern NSString *did_select_sync_tab;
+extern NSString *did_update_last_row;
+extern NSString *user_metric_prefereces_changed;
+
+void set_nimrod_metric_use_based_on_user_preferences(void);
 NSString *format_date(TWeight *weight);
 NSString *format_nsdate(NSDate *date);
--- src/EHAppDelegate.m
+++ src/EHAppDelegate.m
@@ -2,8 +2,23 @@
 
 #import "EHHistory_vc.h"
 #import "categories/NSString+seohyun.h"
+#import "user_config.h"
 
 #import "ELHASO.h"
+
+
+NSString *decimal_separator_changed = @"decimal_separator_changed";
+NSString *did_accept_file = @"NSNotificationDidAcceptFile";
+NSString *did_accept_file_path = @"NSNotificationDidAcceptFilePath";
+NSString *did_add_row = @"NSNotificationDidAddRow";
+NSString *did_add_row_pos = @"NSNotificationDidAddRowPos";
+NSString *did_change_changelog_version = @"NSNotificationDidChangeLogVersion";
+NSString *did_import_csv = @"NSNotificationDidImportCSV";
+NSString *did_remove_row = @"NSNotificationDidRemoveRow";
+NSString *did_select_sync_tab = @"NSNotificationDidSelectSyncTab";
+NSString *did_update_last_row = @"NSNotificationDidUpdateLastRow";
+NSString *user_metric_prefereces_changed = @"user_metric_preferences_changed";
+
 
 @interface EHAppDelegate ()
 
@@ -19,9 +34,22 @@
     NSString *db_path = get_path(@"", DIR_LIB);
     DLOG(@"Setting database path to %@", db_path);
 
+    [self generate_changelog_timestamp_if_empty:db_path];
+
     if (!open_db([db_path cstring]))
         abort();
     DLOG(@"Got %lld entries", get_num_weights());
+
+    // Obtain metric setting from environment.
+    // http://stackoverflow.com/a/9997513/172690
+    NSLocale *locale = [NSLocale autoupdatingCurrentLocale];
+    const BOOL uses_metric = [[locale objectForKey:NSLocaleUsesMetricSystem]
+        boolValue];
+    NSString *separator = [locale objectForKey:NSLocaleDecimalSeparator];
+
+    DLOG(@"Uses metric? %d, decimal separator is '%@'", uses_metric, separator);
+    set_decimal_separator([separator cstring]);
+    set_nimrod_metric_use_based_on_user_preferences();
 
     // Insert code here to initialize your application
     self.history_vc = [[EHHistory_vc alloc]
@@ -31,7 +59,37 @@
     self.history_vc.view.frame = ((NSView*)self.window.contentView).bounds;
 }
 
+/** Generates user preferences timestamp for changelog version.
+ *
+ * If the user doesn't have yet a version value for seen changelog file, this
+ * function creates it based on the current bundle version, so as to not nag
+ * new users with changes they likely are not interested in.
+ */
+- (void)generate_changelog_timestamp_if_empty:(NSString*)db_path
+{
+    if (config_changelog_version() > 0)
+        return;
+
+    DLOG(@"DB doesn't exist, setting changelog version to %0.1f",
+        bundle_version());
+    set_config_changelog_version(bundle_version());
+}
+
 @end
+
+#pragma mark -
+#pragma mark Global functions
+
+/// Helper method to update nimrod's global metric defaults.
+void set_nimrod_metric_use_based_on_user_preferences(void)
+{
+    //const int pref = user_metric_preference();
+    const int pref = 0;
+    if (pref > 0)
+        specify_metric_use((1 == pref));
+    else
+        specify_metric_use(system_uses_metric());
+}
 
 /** Wraps format_nsdate with a TWeight* accessor.
  *
